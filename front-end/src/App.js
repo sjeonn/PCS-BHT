686b5b8bbd096ad392d04319b1593045777afc92
Lucas Schaberg <lucasmschaberg@gmail.com>
2021-11-05 01:24:28 -0700

    add first version of listings

---
+++ front-end/src/App.js
@@ -0,0 +1,22 @@
+import Listings from './Listings';
+
+const listing1 = {
+  "id" : 1,
+  "link" : "https://google.com",
+  "img" : "https://www.thesprucepets.com/thmb/kV_cfc9P4QWe-klxZ8y--awxvY4=/960x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/adorable-white-pomeranian-puppy-spitz-921029690-5c8be25d46e0fb000172effe.jpg",
+  "location" : "The Moon",
+  "bedrooms" : 27,
+  "price" : 420
+}
+
+const listings = [listing1, listing1, listing1, listing1, listing1, listing1]
+
+function App() {
+  return (
+    <div className="App">
+      <Listings listings={listings}/>
+    </div>
+  );
+}
+
+export default App;
