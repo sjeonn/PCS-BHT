4957fac208f61054e2a3c81f660940e58b107681
Ashvin Dhawan <ashvindhawan@berkeley.edu>
2021-11-18 18:28:15 -0800

    updated create_data call in migrations

--- backend/bhs_api_container/wrappers/craig.py
+++ backend/bhs_api_container/wrappers/craig.py
@@ -1,7 +1,7 @@
 from craigslist import CraigslistHousing
 import sys
-sys.path.append("../scrapper")
-from models import Listing #this needs work 
+# sys.path.append("../scrapper")
+# from models import Listing #this needs work 
 
 def get_listings():
     data = []
@@ -9,6 +9,9 @@ def get_listings():
                          filters={'zip_code': "94704", 'search_distance':"1", 'max_price': 1200, 'private_room': True})
                          
     for result in cl_h.get_results(sort_by='newest', geotagged=True):
-        temp = Listing(address = result['where'], email = "", price = result['price'], rooms = "", photo = "https://sfbay.craigslist.org/eby/roo/d/berkeley-lovely-private-sunny-very/7405635388.html")
-        data.append(temp)
-    return data
+        print(result)
+    #     temp = Listing(address = result['where'], email = "", price = result['price'], rooms = "", photo = "https://sfbay.craigslist.org/eby/roo/d/berkeley-lovely-private-sunny-very/7405635388.html")
+    #     data.append(temp)
+    # return data
+
+get_listings()
