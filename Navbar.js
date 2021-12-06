e98bb018d5d9515c9e9154fe6c5d251299096ee5
ritiksinha1 <47905630+ritiksinha1@users.noreply.github.com>
2021-12-05 17:35:30 -0800

    Added Resources Page

---
+++ Navbar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Navbar.css'
+
+function Navbar() {
+    return (
+        <header class = "header"> 
+            <div class = "title">
+                <h1><a href = "/">The Berkeley Housing Tool</a></h1>
+            </div>
+            <nav class = "links">
+                <ul>
+                    <li><a href = "listings"> Listings </a></li>
+                    <li><a href = "resources"> Resources </a></li>
+                    <li><a href = "#"> About Us </a></li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
+
+export default Navbar;
---
+++ Resources.js
@@ -0,0 +1,18 @@
+import './Resources.css';
+
+function Resources() {
+    return (
+        <div class = "main">
+            <h1>Housing Resources</h1>
+            <ul>
+                <li> Lorem Ipsum #1 - Lorem ipsum dolor sit amet, consectetur adipiscing </li>
+                <li> Lorem Ipsum #2 - Lorem ipsum dolor sit amet, consectetur adipiscing </li>
+                <li> Lorem Ipsum #3 - Lorem ipsum dolor sit amet, consectetur adipiscing </li>
+                <li> Lorem Ipsum #4 - Lorem ipsum dolor sit amet, consectetur adipiscing </li>
+                <li> Lorem Ipsum #5 - Lorem ipsum dolor sit amet, consectetur adipiscing </li>
+            </ul>
+        </div>
+    );
+  }
+  
+export default Resources;
