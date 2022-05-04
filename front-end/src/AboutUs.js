4942e0437f9d6bfe8982b968d299ba6d0f282b8a
Sungjoo Jeon <sungjoojeon@berkeley.edu>
2022-05-03 17:15:35 -0700

    Revert "FINISHED!"
    This reverts commit 5279eef20f6ed45dfa6b67ccb3d7d9b34569372b.

--- front-end/src/AboutUs.js
+++ front-end/src/AboutUs.js
@@ -6,12 +6,8 @@ function Resources() {
         <div class = "main">
             <img src = {pcs_img} />
             <h2> A Political Computer Science @ Berkeley Project </h2>
-            <p> Created by Sage Jeon, Abdul Karim, Ashvin Dhawan, Lucas Schaberg, Owen Long, Ritik Sinha, </p>
-            <p>Rohil Bhinge, Lilian Chang, Aneesh Durai, Arjun Gadiyar, and Soochang Choi.</p>
-            <br></br>
-            <p> Political Computer Science at Berkeley provides students with a space to tap into both of these backgrounds.  </p>
-            <p>We are a community of students interested in computer and political science and more specifically, the intersection of these two important fields. </p>
-            <p id="pcs_link"> <a target = "_blank" href = "https://pcs.berkeley.edu/"> LEARN MORE </a> </p>
+            <p> Created by Sage Jeon, Abdul Karim, Ashvin Dhawan, Lucas Schaberg, Owen Long, Ritik Sinha, Rohil Bhinge, Lilian Chang, Aneesh Durai, Arjun Gadiyar, and Soochang Choi. </p>
+            <p id="pcs_link"> <a target = "_blank" href = "https://pcs.berkeley.edu/"> PCS Link </a> </p>
         </div>
     );
   }
